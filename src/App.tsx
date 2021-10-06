c19cca9c325cd24e24739b9b7cd8e7946aa46032
leandronunesdev <nunes.pessoal@gmail.com>
2021-10-06 09:35:59 -0300

    atualização do texto, correção do favicon e reordenamento dos projetos

--- src/App.tsx
+++ src/App.tsx
@@ -8,126 +8,129 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import { Perfil } from './types/perfil';
 
-import LogoReact from '../src/images/reactjs.svg'
-import LogoTypeScript from '../src/images/typescript.svg'
-import LogoRedux from '../src/images/redux.svg'
-import LogoJavaScript from '../src/images/javascript.svg'
-import LogoCSS from '../src/images/css3.svg'
-import LogoHTML from '../src/images/html5.svg'
+import LogoReact from '../src/images/reactjs.svg';
+import LogoTypeScript from '../src/images/typescript.svg';
+import LogoRedux from '../src/images/redux.svg';
+import LogoJavaScript from '../src/images/javascript.svg';
+import LogoCSS from '../src/images/css3.svg';
+import LogoHTML from '../src/images/html5.svg';
 
-import ProjectFoodShop from '../src/images/food-shop.png'
-import ProjectCurriculo from '../src/images/curriculo-typescript.png'
-import ProjectInstagram from '../src/images/instagram-clone.png'
-import ProjectTwitter from '../src/images/twitter-clone.png'
-import ProjectTransportadora from '../src/images/transportadora.png'
-import ProjectPortfolio from '../src/images/portfolio.png'
-import ProjectEmporio from '../src/images/emporio.png'
+import ProjectFoodShop from '../src/images/food-shop.png';
+import ProjectCurriculo from '../src/images/curriculo-typescript.png';
+import ProjectInstagram from '../src/images/instagram-clone.png';
+import ProjectTwitter from '../src/images/twitter-clone.png';
+import ProjectTransportadora from '../src/images/transportadora.png';
+import ProjectPortfolio from '../src/images/portfolio.png';
+import ProjectEmporio from '../src/images/emporio.png';
 
 function App() {
-
-  const skillsAPI =
-  {
+  const skillsAPI = {
     skills: [
       {
         id: 1,
-        name: "ReactJS",
-        image: LogoReact          
+        name: 'ReactJS',
+        image: LogoReact,
       },
       {
         id: 2,
-        name: "TypeScript",
-        image: LogoTypeScript          
+        name: 'TypeScript',
+        image: LogoTypeScript,
       },
       {
         id: 3,
-        name: "Redux",
-        image: LogoRedux          
+        name: 'Redux',
+        image: LogoRedux,
       },
       {
         id: 4,
-        name: "JavaScript",
-        image: LogoJavaScript          
+        name: 'JavaScript',
+        image: LogoJavaScript,
       },
       {
         id: 5,
-        name: "CSS3",
-        image: LogoCSS          
+        name: 'CSS3',
+        image: LogoCSS,
       },
       {
         id: 6,
-        name: "HTML5",
-        image: LogoHTML
-      }
+        name: 'HTML5',
+        image: LogoHTML,
+      },
     ],
     projects: [
       {
+        id: 5,
+        title: 'Site Institucional',
+        description:
+          'Trabalho Freelance realizado para a agência Imaggine de site institucional de transportadora. Site em Wordpress com customizações do CSS',
+        image: ProjectTransportadora,
+        codeLink: '',
+        liveLink: 'http://awcompany.com.br/',
+      },
+      {
         id: 7,
-        title: "Empório da Cerveja",
-        description: "Projeto baseado no site da Ambev de mesmo nome. Consumo de API através de autenticação via Token. Simulação de um carrinho de compras. Tecnologias utilizadas: React, TypeScript e Redux.",
-        image: ProjectEmporio,        
-        codeLink: "https://github.com/leandronunesdev/emporio",
-        liveLink: ""
+        title: 'Empório da Cerveja',
+        description:
+          'Projeto baseado no site da Ambev de mesmo nome. Consumo de API através de autenticação via Token. Simulação de um carrinho de compras. Tecnologias utilizadas: React, TypeScript e Redux.',
+        image: ProjectEmporio,
+        codeLink: 'https://github.com/leandronunesdev/emporio',
+        liveLink: '',
       },
       {
         id: 1,
-        title: "Instagram Clone",
-        description: "Projeto de Instagram Clone utilizando React, TypeScript e Redux.",
-        image: ProjectInstagram,        
-        codeLink: "https://github.com/leandronunesdev/instagram-clone2",
-        liveLink: ""
+        title: 'Instagram Clone',
+        description:
+          'Projeto de Instagram Clone utilizando React, TypeScript e Redux.',
+        image: ProjectInstagram,
+        codeLink: 'https://github.com/leandronunesdev/instagram-clone2',
+        liveLink: '',
       },
       {
         id: 2,
-        title: "Food Shop",
-        description: "Projeto de Loja Online usando consumo de API. Ferramentas: React e TypeScript.",
-        image: ProjectFoodShop,        
-        codeLink: "https://github.com/leandronunesdev/food-shop",
-        liveLink: "https://leandronunesdev.github.io/food-shop/"
+        title: 'Food Shop',
+        description:
+          'Projeto de Loja Online usando consumo de API. Ferramentas: React e TypeScript.',
+        image: ProjectFoodShop,
+        codeLink: 'https://github.com/leandronunesdev/food-shop',
+        liveLink: 'https://leandronunesdev.github.io/food-shop/',
       },
       {
         id: 3,
-        title: "Currículo Online",
-        description: "Projeto de Currículo Online utilizando React e TypeScript.",
-        image: ProjectCurriculo,        
-        codeLink: "https://github.com/leandronunesdev/curriculo-typescript2",
-        liveLink: "https://leandronunesdev.github.io/curriculo-typescript2/"
+        title: 'Currículo Online',
+        description:
+          'Projeto de Currículo Online utilizando React e TypeScript.',
+        image: ProjectCurriculo,
+        codeLink: 'https://github.com/leandronunesdev/curriculo-typescript2',
+        liveLink: 'https://leandronunesdev.github.io/curriculo-typescript2/',
       },
       {
         id: 4,
-        title: "Twitter Clone",
-        description: "Projeto de Twitter Clone utilizando Classes ES6",
-        image: ProjectTwitter,        
-        codeLink: "https://github.com/leandronunesdev/twitter-clone2",
-        liveLink: "https://leandronunesdev.github.io/twitter-clone2/"
-      },
-      {
-        id: 5,
-        title: "Site Institucional",
-        description: "Trabalho Freelance realizado para a agência Imaggine de site institucional de transportadora. Site em Wordpress com customizações do CSS",
-        image: ProjectTransportadora,        
-        codeLink: "",
-        liveLink: "http://awcompany.com.br/"
+        title: 'Twitter Clone',
+        description: 'Projeto de Twitter Clone utilizando Classes ES6',
+        image: ProjectTwitter,
+        codeLink: 'https://github.com/leandronunesdev/twitter-clone2',
+        liveLink: 'https://leandronunesdev.github.io/twitter-clone2/',
       },
       {
         id: 6,
-        title: "Portfolio",
-        description: "Site de portfolio pessoal utilizando React, TypeScript e SASS",
-        image: ProjectPortfolio,        
-        codeLink: "https://github.com/leandronunesdev/portfolio-2021",
-        liveLink: "https://leandronunes.dev/"
-      }
-    ]
-  }
+        title: 'Portfolio',
+        description:
+          'Site de portfolio pessoal utilizando React, TypeScript e SASS',
+        image: ProjectPortfolio,
+        codeLink: 'https://github.com/leandronunesdev/portfolio-2021',
+        liveLink: 'https://leandronunes.dev/',
+      },
+    ],
+  };
 
-const [resposta] = useState<Perfil>(skillsAPI)
-
+  const [resposta] = useState<Perfil>(skillsAPI);
 
   return (
-    <div className="App">
-      <Header />   
-      <Hero />   
-      <Skills dados={resposta}/>
-      <Projects dados={resposta}/>
+    <div className='App'>
+      <Header />
+      <Hero />
+      <Skills dados={resposta} />
+      <Projects dados={resposta} />
       <Contact />
       <Footer />
     </div>
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,27 +1,38 @@
-import profile_pic from '../../images/profile-picture_square.jpg'
+import profile_pic from '../../images/profile-picture_square.jpg';
 
-import './Hero.scss'
+import './Hero.scss';
 
 const Hero = () => {
   return (
-    <div className="hero-box">
-      <div className="hero-image responsive">
-        <img src={profile_pic} alt="Leandro"/>
+    <div className='hero-box'>
+      <div className='hero-image responsive'>
+        <img src={profile_pic} alt='Leandro' />
       </div>
-      <div className="hero-text">
-        <h1>Leandro Nunes,<br/> Desenvolvedor Front-End</h1>
-        <p>Analista de Compliance em transição de carreira para Desenvolvedor Front-End. 
-          Possuo conhecimento em ReactJS, TypeScript, Redux, HTML5, CSS3 e JavaScript.
-          Tenho visão crítica e analítica, capacidade de planejamento, organização e senso de urgência.
-          Sou cooperativo, multiplicador de conhecimento, tenho facilidade de comunicação oral e escrita e nível avançado de inglês.          
+      <div className='hero-text'>
+        <h1>
+          Leandro Nunes,
+          <br /> Desenvolvedor Front-End
+        </h1>
+        <p>
+          Desenvolvedor Front-End com conhecimento em ReactJS, React Native,
+          TypeScript, ContextAPI, Redux, JavaScript e Styled Components. Tenho
+          visão crítica e analítica, capacidade de planejamento, organização e
+          senso de urgência. Sou cooperativo, multiplicador de conhecimento,
+          tenho facilidade de comunicação oral e escrita e nível avançado de
+          inglês.
         </p>
-        <a href="https://github.com/leandronunesdev/curriculo/raw/master/LEANDRO%20NUNES%20-%20DEV.pdf" target="_blank"><button className="button-styled">Baixar Currículo</button></a>
+        <a
+          href='https://github.com/leandronunesdev/curriculo/raw/master/LEANDRO%20NUNES%20-%20DEV.pdf'
+          target='_blank'
+        >
+          <button className='button-styled'>Baixar Currículo</button>
+        </a>
       </div>
-      <div className="hero-image desktop">
-        <img src={profile_pic} alt="Leandro"/>
+      <div className='hero-image desktop'>
+        <img src={profile_pic} alt='Leandro' />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
